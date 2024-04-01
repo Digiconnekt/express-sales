debfc553b4e1d6ab6627ebbf917a511dd11c3557
Rohan Daddekar <rohandaddekar9@gmail.com>
2024-04-01 16:56:54 +0530

    removed types

---
+++ src/components/MobileMenu/mobile-menu.js
@@ -0,0 +1,22 @@
+import { slideUp, slideDown } from "../../utils/helper";
+
+const linkTo = (menu, navigate, setActiveMobileMenu) => {
+  if (menu.subMenu) {
+    menu.activeDropdown = !menu.activeDropdown;
+  } else {
+    if (menu.pathname !== undefined) {
+      setActiveMobileMenu(false);
+      navigate(menu.pathname);
+    }
+  }
+};
+
+const enter = (el) => {
+  slideDown(el, 300);
+};
+
+const leave = (el) => {
+  slideUp(el, 300);
+};
+
+export { linkTo, enter, leave };
