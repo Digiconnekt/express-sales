8c2c2ac4b615df2263f9a03cc7c052aabfb7b100
Rohan Daddekar <rohandaddekar9@gmail.com>
2024-05-02 16:39:46 +0530

    filter updated, breadcrumb added

--- src/dashboards/StoreDashboard.jsx
+++ src/dashboards/StoreDashboard.jsx
@@ -4,10 +4,28 @@ import { useSelector } from "react-redux";
 const StoreDashboard = ({ children, storeId }) => {
   const user = useSelector((state) => state.auth.user);
 
-  if (user && user.role === "store-manager" && storeId) {
-    return <Navigate to={`/store`} />;
-  } else {
-    return <>{children}</>;
+  if (user && user.role === "master") {
+    if (!storeId) {
+      return <Navigate to={`/`} />;
+    } else {
+      return <>{children}</>;
+    }
+  }
+
+  if (user && user.role === "company-manager") {
+    if (!storeId) {
+      return <Navigate to={`/`} />;
+    } else {
+      return <>{children}</>;
+    }
+  }
+
+  if (user && user.role === "store-manager") {
+    if (storeId) {
+      return <Navigate to={`/store`} />;
+    } else {
+      return <>{children}</>;
+    }
   }
 };
 
